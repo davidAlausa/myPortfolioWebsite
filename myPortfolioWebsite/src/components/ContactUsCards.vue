cdd04b0cd0917f5e0eaec18ae3740afcb7b3adfb
K00270915 <damialausa@gmail.com>
2025-08-18 23:43:41 +0100

    refactor: removed toggle functionality in contact us page to fix bug

--- myPortfolioWebsite/src/components/ContactUsCards.vue
+++ myPortfolioWebsite/src/components/ContactUsCards.vue
@@ -1,23 +1,15 @@
 <script setup>
-    import { defineProps, ref } from 'vue';
+    import { defineProps } from 'vue';
     
     defineProps({
         LOGO: String,
     });
 
-    const isOpen = ref(false)
-    const toggle = () => {
-    isOpen.value = !isOpen.value
-}
-
 </script>
 <template>
-    <Card @click="toggle" class=" flex gap-x-20 items-center hover:justify-between bg-accentDARK hover:bg-secondaryDARK rounded-full p-8 mb-5 shadow-lg  max-w-[90px] md:max-w-[125px] hover:max-w-[1400px] 
+    <Card class="flex gap-x-20 items-center hover:justify-between bg-accentDARK hover:bg-secondaryDARK rounded-full p-8 mb-5 shadow-lg  max-w-[90px] md:max-w-[125px] hover:max-w-[1400px] 
            transition-all duration-500 ease-in-out group">
 
-        <div :class="[ 'transition-all duration-500 ease-in-out', isOpen ? 'block' : 'hidden', 'md:hidden' ]">
-            <slot></slot>
-        </div>
         <div class="hidden group-hover:block md:group-hover:truncate">
             <slot></slot>
         </div>
