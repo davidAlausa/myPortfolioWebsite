b415c7587472eb949605f4c1417050b2e15e68c4
K00270915 <damialausa@gmail.com>
2025-08-18 21:42:48 +0100

    feat: fixed toggle functionality to ContactUsCards component for improved interactivity on mobile devices

--- myPortfolioWebsite/src/components/ContactUsCards.vue
+++ myPortfolioWebsite/src/components/ContactUsCards.vue
@@ -1,14 +1,23 @@
 <script setup>
-    import { defineProps } from 'vue';
+    import { defineProps, ref } from 'vue';
     
     defineProps({
         LOGO: String,
     });
 
+    const isOpen = ref(false)
+    const toggle = () => {
+    isOpen.value = !isOpen.value
+}
+
 </script>
 <template>
-    <Card class=" flex gap-x-20 items-center hover:justify-between bg-accentDARK hover:bg-secondaryDARK rounded-full p-8 mb-5 shadow-lg  max-w-[90px] md:max-w-[125px] hover:max-w-[1400px] 
+    <Card @click="toggle" class=" flex gap-x-20 items-center hover:justify-between bg-accentDARK hover:bg-secondaryDARK rounded-full p-8 mb-5 shadow-lg  max-w-[90px] md:max-w-[125px] hover:max-w-[1400px] 
            transition-all duration-500 ease-in-out group">
+
+        <div :class="[ 'transition-all duration-500 ease-in-out', isOpen ? 'block' : 'hidden', 'md:hidden' ]">
+            <slot></slot>
+        </div>
         <div class="hidden group-hover:block md:group-hover:truncate">
             <slot></slot>
         </div>
