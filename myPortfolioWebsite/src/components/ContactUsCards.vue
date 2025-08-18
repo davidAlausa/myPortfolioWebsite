f60844cc5debca898d0449522d09df57870bedf1
K00270915 <damialausa@gmail.com>
2025-08-19 00:12:42 +0100

    refactor: update ContactUsCards component styles for improved visual consistency and responsiveness

--- myPortfolioWebsite/src/components/ContactUsCards.vue
+++ myPortfolioWebsite/src/components/ContactUsCards.vue
@@ -7,12 +7,16 @@
 
 </script>
 <template>
-    <Card class="flex gap-x-20 items-center hover:justify-between bg-accentDARK hover:bg-secondaryDARK rounded-full p-8 mb-5 shadow-lg  max-w-[90px] md:max-w-[125px] hover:max-w-[1400px] 
+    <Card class="flex gap-x-20 items-center hover:justify-between bg-none md:bg-accentDARK md:hover:bg-secondaryDARK rounded-full p-8 mb-5 md:shadow-lg  max-w-[fit] md:max-w-[125px] hover:max-w-[1400px] 
            transition-all duration-500 ease-in-out group">
 
-        <div class="hidden group-hover:block md:group-hover:truncate">
+        <div class="md:hidden md:group-hover:block md:group-hover:truncate">
             <slot></slot>
         </div>
-        <i :class="`pi ${LOGO} text-primaryDARK text-3xl md:text-6xl transition-transform duration-500 ease-in-out`"></i>
+        <i :class="`pi ${LOGO} text-secondaryDark md:text-primaryDARK text-3xl md:text-6xl transition-transform duration-500 ease-in-out`"></i>
     </Card>
+
+    <div class="md:hidden px-20">
+        <hr class="my-6 border-t-2 border-accentDARK" />
+    </div>
 </template>
--- myPortfolioWebsite/src/views/ContactView.vue
+++ myPortfolioWebsite/src/views/ContactView.vue
@@ -7,20 +7,20 @@
     <h1 class="text-7xl font-bold mt-5 md:mt-10 mb-10 text-accentDARK">Contact</h1>
     <ContactUsCards LOGO="pi-github flex gap-x-20">
         <a href="https://www.github.com/davidAlausa">   
-        <h1 class="text-3xl font-bold text-primaryDARK">GitHub</h1>
-        <p class="text-primaryDARK">Explore my projects and contributions on GitHub.</p>
+        <h1 class="text-3xl font-bold text-secondaryDARK md:text-primaryDARK">GitHub</h1>
+        <p class="text-secondaryDARK md:text-primaryDARK">Explore my projects and contributions on GitHub.</p>
         </a>
     </ContactUsCards>
     <ContactUsCards LOGO="pi-linkedin">
         <a href="https://www.linkedin.com/in/david-alausa-251405290">
-        <h1 class="text-3xl font-bold text-primaryDARK">LinkedIn</h1>
-        <p class="text-primaryDARK">Connect with me on LinkedIn to see my professional journey.</p>
+        <h1 class="text-3xl font-bold text-secondaryDARK md:text-primaryDARK">LinkedIn</h1>
+        <p class="text-secondaryDARK md:text-primaryDARK">Connect with me on LinkedIn to see my professional journey.</p>
         </a>
     </ContactUsCards>
     <ContactUsCards LOGO="pi-envelope">
         <a href="mailto:damialausa@gmail.com">
-        <h1 class="text-3xl font-bold text-primaryDARK">Email</h1>
-        <p class="text-primaryDARK">Feel free to reach out via email for any inquiries or collaborations.</p>   
+        <h1 class="text-3xl font-bold text-secondaryDARK md:text-primaryDARK">Email</h1>
+        <p class="text-secondaryDARK md:text-primaryDARK">Feel free to reach out via email for any inquiries or collaborations.</p>   
         </a>
     </ContactUsCards>
 </div>
